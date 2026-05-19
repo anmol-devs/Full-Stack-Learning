3fd9ecc47b6f35c2f8f8e73d2dc8a6f06209def1
Anmol-24-S <bhamrahanmol100@gmail.com>
2026-05-19 19:22:28 +0530

    async await

--- My Things/JS/JS Async/Await & Fetch API/script.js
+++ My Things/JS/JS Async/Await & Fetch API/script.js
@@ -7,15 +7,15 @@
 //     })
 // }
 
-// settle means resolve or reject
+// promise ke settle hone ka mtlb resolve or reject
 // resolve means promise has settled successfully
 // reject means promise has not settled successfully
 
 async function getData() {
     // Simulate getting data from a server
-    // let x = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    // let x = await fetch('https://jsonplaceholder.typicode.com/todos/1') // fetch ke andr ye fake api ka url hai, online mil jaaega.
 
-    let x = await fetch('https://jsonplaceholder.typicode.com/posts', {
+    let x = await fetch('https://jsonplaceholder.typicode.com/posts', { // by default fetch method is GET, but we can change it to POST, PUT, DELETE etc.
                 method: 'POST',
                 body: JSON.stringify({
                     title: 'foo',
@@ -26,7 +26,9 @@ async function getData() {
                     'Content-type': 'application/json; charset=UTF-8',
                 },
             })
-    let data = await x.json() 
+    let data = await x.json() // x.json() is also a promise, so we need to await it as well to get the actual data. And ye x.json() method is used to parse the response data as JSON, because fetch returns a Response object, and we need to convert it to JSON format to work with it easily in JavaScript.
+    // data chunks me aaega and fir use parse karke hme ek complete data milega. Await nhi krenge to sirf promise milega, na hi json na hi text milega.
+    // apne x.text() bhi use kar sakte hai, agar hume text format me data chahiye to. But json() method is more commonly used when working with APIs that return JSON data.
     return data
 }
 
@@ -37,7 +39,7 @@ async function main(){
 
     console.log("Load data")
 
-    let data = await getData()
+    let data = await getData()  // await use krne ke liye async ke andr hona chahiye.
 
     console.log(data)
 
